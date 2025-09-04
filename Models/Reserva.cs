67b395431e0bb899d701e9e65e86dee8ac03d5da
pedrxtxrres <pedrodealmeida1@hotmail.com>
2025-09-04 18:17:33 -0300

    Salvando classe reserva

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -8,11 +8,9 @@ namespace ProjetoHospedagemHotel.Models
     public class Reserva
     {
         public List<Pessoa> Hospedes { get; set; }
-        public Suite Suite { get; set; }
+        public List<Suite> Suite { get; set; }
         public int DiasReservados { get; set; }
 
-        List<Pessoa> ListaHospedes = new List<Pessoa>();
-        List<Suite> ListaSuites = new List<Suite>();
 
         public void PausarTela()
         {
@@ -21,19 +19,20 @@ namespace ProjetoHospedagemHotel.Models
         }
         public void CadastrarHospedes(Pessoa pessoa)
         {
-            ListaHospedes.Add(pessoa);
+            Hospedes.Add(pessoa);
             Console.WriteLine($"{pessoa.Nome} {pessoa.Sobrenome} adicionado com sucesso!");
             PausarTela();
         }
         public void CadastrarSuite(Suite suite)
         {
-            ListaSuites.Add(suite);
+            Suite.Add(suite);
             Console.WriteLine($"{suite.NomeDaSuite} adicionada com sucesso!");
             PausarTela();
         }
         public int ObterQuantidadeHospedes()
         {
-            return 0;
+            int QuantidadeDeHospedes = Hospedes.Count;
+            return QuantidadeDeHospedes;
         }
         public decimal CalcularValorDiaria()
         {
